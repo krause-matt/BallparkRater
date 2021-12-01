5329727c2919a7d0d0ddda2a520c4f1367ebd0ab
Matt Krause <krause.matt@pm.me>
2021-11-30 21:21:19 -0500

    Setting up basic structure for app using express

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
-const Campground = require('./models/campground');
+const Ballpark = require('./models/ballparks');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -29,40 +29,40 @@ app.use(methodOverride('_method'));
 app.get('/', (req, res) => {
     res.render('home')
 });
-app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+app.get('/ballparks', async (req, res) => {
+    const ballparks = await Ballpark.find({});
+    res.render('ballparks/index', { ballparks })
 });
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
+app.get('/ballparks/new', (req, res) => {
+    res.render('ballparks/new');
 })
 
-app.post('/campgrounds', async (req, res) => {
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
+app.post('/ballparks', async (req, res) => {
+    const ballpark = new ballpark(req.body.ballpark);
+    await ballpark.save();
+    res.redirect(`/ballparks/${ballpark._id}`)
 })
 
-app.get('/campgrounds/:id', async (req, res,) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground });
+app.get('/ballparks/:id', async (req, res,) => {
+    const ballpark = await Ballpark.findById(req.params.id)
+    res.render('ballparks/show', { ballpark });
 });
 
-app.get('/campgrounds/:id/edit', async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
+app.get('/ballparks/:id/edit', async (req, res) => {
+    const ballpark = await Ballpark.findById(req.params.id)
+    res.render('ballparks/edit', { ballpark });
 })
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/ballparks/:id', async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
+    const ballpark = await Ballpark.findByIdAndUpdate(id, { ...req.body.ballpark });
+    res.redirect(`/ballparks/${ballpark._id}`)
 });
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/ballparks/:id', async (req, res) => {
     const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
+    await Ballpark.findByIdAndDelete(id);
+    res.redirect('/ballparks');
 })
 
 
--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
-const Campground = require('../models/campground');
+const Campground = require('../models/ballparks');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect('mongodb://localhost:27017/ballparks', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -20,14 +20,14 @@ const sample = array => array[Math.floor(Math.random() * array.length)];
 
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
+    await Ballpark.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
-        const camp = new Campground({
+        const park = new Ballpark({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`
         })
-        await camp.save();
+        await park.save();
     }
 }
 
