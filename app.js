cf369c71da5b5bd6c9992cd16ce3c25bc9427e1e
Matt Krause <krause.matt@pm.me>
2022-01-04 21:57:23 -0500

    Making ballpark details page responsive, collapse columns under 1200px screen width

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const ExpressErr = require("./utilities/ExpressErr");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const mongoSanitize = require("express-mongo-sanitize");
+const helmet = require("helmet");
 
 const User = require("./models/user");
 
@@ -43,8 +44,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(mongoSanitize());
+//app.use(helmet({contentSecurityPolicy: false,}));
 
 const sessionSettings = {
+    name: "BR_Session",
     secret: "Comiskey",
     resave: false,
     saveUninitialized: true,
